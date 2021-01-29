c7eebae88c3ea3ddf5b6503fb53aa9c62450dffd
Valeria <sokolovalera73_04@mail.ru>
2021-01-29 22:46:34 +0300

    эх

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModelsService } from '../models.service';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,10 @@ export class HomeComponent implements OnInit {
 
   models: any[] = [] 
   
-  constructor(private router: Router) { }
-
-  goToBasket() {
-    this.router.navigate(["/basket"]);
-  }
+  constructor(private router: Router, private modelsService: ModelsService) {
+    this.models = this.modelsService.telescopes
+    this.models.push(...this.modelsService.microscopes)
+   }
 
   ngOnInit(): void {
   }
